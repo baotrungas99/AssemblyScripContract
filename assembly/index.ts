5bcb224b8dc81f79d4d5edb676a2e728926db077
Chris Nguyen <baotrungas99@gmail.com>
2023-01-28 10:59:50 +0000

    deploy

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,8 +1,8 @@
 import {Context, storage} from "near-sdk-as";
 
 class Greating {
-    getGreeting(accounId : string): string | null {
-        return storage.get<string>(accounId, "Greeting not available");
+    getGreeting(accountId : string): string | null {
+        return storage.get<string>(accountId, "Greeting not available");
     }
     setGreeting(greeting: string): void {
         storage.set(Context.sender, greeting);
